8d9412ac0f167df46030373435f9265410a4c303
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2024-01-24 13:15:17 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BuildSystem
 A Plugin build system for Minecraft Servers.
+
+---
+# What do you need?
+
+- Java 17
+- Paper API 1.20.4
---
+++ src/main/java/de/murmelmeister/system/Build.java
@@ -0,0 +1,20 @@
+package de.murmelmeister.system;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Build extends JavaPlugin {
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
